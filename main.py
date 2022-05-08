5a6ea28184c8531c8ab23c43d078a69d90a482d0
unknown <kyesiltepe@gmail.com>
2022-05-08 20:16:16 +0300

    day-48-selenium study first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+
+chrome_driver_path=r"c:\Development\chromedriver.exe"
+s=Service(executable_path=chrome_driver_path)
+
+# driver=webdriver.Chrome(executable_path=chrome_driver_path)
+driver=webdriver.Chrome(service=s)
+driver.get("https://www.amazon.com.tr/Apple-%C3%A7ekirdekli-14-%C3%A7ekirdekli-GPUya-sahip-Apple-M1/dp/B09JR725DK?ref_=Oct_d_omwf_d_12601898031&pd_rd_w=5rB6v&pf_rd_p=b12ee757-72bc-41fe-a493-67b5c042e229&pf_rd_r=N9TZJZSWAVES3MD53V45&pd_rd_r=6bce771b-e5ca-411a-b70f-f29600f1290a&pd_rd_wg=lt0PO&pd_rd_i=B09JR725DK")
+# price=driver.find_element_by_id("attach-base-product-price")
+price=driver.find_element(by=By.ID,value="attach-base-product-price") #difficult to find this id,very hard
+print(price.get_attribute("value"))
+# driver.close()
+driver.quit()
